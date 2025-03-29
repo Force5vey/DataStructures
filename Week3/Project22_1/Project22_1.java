87e65bb368a726de60edf5bd6657e680f56f145a
EJ <ej@force5dev.com>
2025-03-29 16:01:04 -0400

    Adjusted to keep last character in current substring so it is available if it is the start of a new longer substring.
    basic use test is success.

--- Week3/Project22_1/Project22_1.java
+++ Week3/Project22_1/Project22_1.java
@@ -20,20 +20,6 @@ public class Project22_1
         System.out.print("Maximum consecutive increasingly ordered substring is ");
         System.out.println(subString);
 
-        // Prompt for input
-        // - ignoring edge cases, just string, ignore anything after any spaces
-        // Variables
-        // - store string
-        // - store substring that is being built during the traversall
-        // - store substring - that is the max one until a longer one is found
-        // Traverse the string
-        // - compare current to previous
-        // - if greater than previous (lexi/alphabetical (will force all to lower)) add to substring (current)
-        // - if not, compare to the max substring, if longer, replace
-        // - reset current and continue traversing
-        // - final comparison at end against stored max substring
-
-        // print results
     }
 }
 
@@ -48,7 +34,7 @@ class StringAnalyzer
 
         for (char c : userString.toCharArray())
         {
-            if (c >= prevChar)
+            if (c > prevChar)
             {
                 currentSubstring += c;
             } else
@@ -58,6 +44,7 @@ class StringAnalyzer
                     maxSubstring = currentSubstring;
 
                     currentSubstring = "";
+                    currentSubstring += c;
                 }
             }
 

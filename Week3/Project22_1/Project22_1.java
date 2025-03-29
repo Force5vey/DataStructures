2f1c3fcf29b5151b7e9b67b4afac144719cb2274
EJ <ej@force5dev.com>
2025-03-29 15:31:02 -0400

    Initial plan laid out.

--- Week3/Project22_1/Project22_1.java
+++ Week3/Project22_1/Project22_1.java
@@ -4,7 +4,20 @@ public class Project22_1
 {
     public static void main(String[] args)
     {
+        // Prompt for input
+        // - ignoring edge cases, just string, ignore anything after any spaces
+        // Variables
+        // - store string
+        // - store substring that is being built during the traversall
+        // - store substring - that is the max one until a longer one is found
+        // Traverse the string
+        // - compare current to previous
+        // - if greater than previous (lexi/alphabetical (will force all to lower)) add to substring (current)
+        // - if not, compare to the max substring, if longer, replace
+        // - reset current and continue traversing
+        // - final comparison at end against stored max substring
 
+        // print results
     }
 
 }

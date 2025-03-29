90359c211e693223b7beea915fb229c7ac7ba2f7
EJ <ej@force5dev.com>
2025-03-29 15:57:33 -0400

    Kind of working, not tracking previous correctly or osmething and is missing a character

--- Week3/Project22_1/Project22_1.java
+++ Week3/Project22_1/Project22_1.java
@@ -1,9 +1,25 @@
 package Project22_1;
 
+import java.util.Scanner;
+
 public class Project22_1
 {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args)
     {
+        System.out.print("Enter a string: ");
+
+        String userInput = sc.nextLine();
+        userInput.toLowerCase();
+
+        StringAnalyzer analyzer = new StringAnalyzer();
+
+        String subString = analyzer.getConsecutiveSubString(userInput);
+
+        System.out.print("Maximum consecutive increasingly ordered substring is ");
+        System.out.println(subString);
+
         // Prompt for input
         // - ignoring edge cases, just string, ignore anything after any spaces
         // Variables
@@ -19,5 +35,35 @@ public class Project22_1
 
         // print results
     }
+}
 
+class StringAnalyzer
+{
+    public String getConsecutiveSubString(String userString)
+    {
+        String currentSubstring = "";
+        String maxSubstring = "";
+
+        char prevChar = 0;
+
+        for (char c : userString.toCharArray())
+        {
+            if (c >= prevChar)
+            {
+                currentSubstring += c;
+            } else
+            {
+                if (currentSubstring.length() > maxSubstring.length())
+                {
+                    maxSubstring = currentSubstring;
+
+                    currentSubstring = "";
+                }
+            }
+
+            prevChar = c;
+        }
+
+        return maxSubstring;
+    }
 }
